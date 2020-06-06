8f0d36e48d2afd92fcdcece2ec0f693064547c56
Deniz Bilgin <deniz@cara.care>
2020-06-06 20:37:23 +0200

    Project init with dependencies & prettier config
    The project was initialised with expo including the initially
    foreseeable dependencies like axios, redux, eslint, prettier
    and typescript. The necessary configurations for prettier and
    eslint were made to increase code consistency and quality
    with auto-formatting feature enabled.

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  env: {
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'react-native/no-inline-styles': 0,
+  },
+};
